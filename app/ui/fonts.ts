80d36d2c11102f7dc6a056dbc8270566852132ef
bstepanek <bstepanek@datasolution.fr>
2024-12-11 10:52:44 +0100

    Starter

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana, Roboto } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+
+export const roboto = Roboto({ weight: ["100" ,"300" , "400" , "500" , "700" , "900"] });
+
+export const lusitana = Lusitana ({ weight: ["400" , "700"] });

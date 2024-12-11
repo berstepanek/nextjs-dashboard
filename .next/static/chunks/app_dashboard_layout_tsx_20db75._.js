d4e40fd323ed94567467de48d4a31718a5f33246
bstepanek <bstepanek@datasolution.fr>
2024-12-11 13:41:17 +0000

    Devcontainer next

---
+++ .next/static/chunks/app_dashboard_layout_tsx_20db75._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_dashboard_layout_tsx_20db75._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_dashboard_layout_tsx_20db75._.js",
+  "chunks": [
+    "static/chunks/_182856._.js"
+  ],
+  "source": "dynamic"
+});
